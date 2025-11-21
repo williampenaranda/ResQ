a0f1575a030de32e2dd39e4ef853384b6c191e5d
williamgit config --global user.name williamgit config --global user.name william <wpenarandat@unicartagena.edu.co>
2025-11-20 20:41:11 -0500

    refactor(estadoEmergencia): remove EN_ESCENA state from emergency status enumeration

--- src/businessLayer/businessEntities/enums/estadoEmergencia.py
+++ src/businessLayer/businessEntities/enums/estadoEmergencia.py
@@ -4,6 +4,5 @@ class EstadoEmergencia(enum.Enum):
     CREADA = "CREADA"
     VALORADA = "VALORADA"
     ASIGNADA = "ASIGNADA"
-    EN_ESCENA = "EN_ESCENA"
     RESUELTA = "RESUELTA"
     CANCELADA = "CANCELADA"

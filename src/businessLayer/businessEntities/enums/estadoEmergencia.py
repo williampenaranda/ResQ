1413c18f7508a30223cc40d7df9e13001f8b4ae7
williamgit config --global user.name williamgit config --global user.name william <wpenarandat@unicartagena.edu.co>
2025-11-20 20:37:07 -0500

    feat(estadoEmergencia): se agrega el estado en escena

--- src/businessLayer/businessEntities/enums/estadoEmergencia.py
+++ src/businessLayer/businessEntities/enums/estadoEmergencia.py
@@ -4,5 +4,6 @@ class EstadoEmergencia(enum.Enum):
     CREADA = "CREADA"
     VALORADA = "VALORADA"
     ASIGNADA = "ASIGNADA"
+    EN_ESCENA = "EN_ESCENA"
     RESUELTA = "RESUELTA"
     CANCELADA = "CANCELADA"
